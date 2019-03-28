7f3473bc32bd8997786408484c4f2633d5964a59
web223dev <ruslanus866@outlook.com>
2019-03-28 15:19:11 +0800

    ajay project

---
+++ js/script.js
@@ -0,0 +1,123 @@
+ $('#myCarousel').carousel({
+  interval: 3000,
+ })
+ 
+ $("#myCarousel").swiperight(function() {
+    $(this).carousel('prev');
+    $(this).carousel('next');
+});
+ 
+$('.dropdown-toggle').dropdown();
+
+ $(document).ready(function () {
+  $('.eventslider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },      
+      767: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+
+  });
+    
+
+
+  $('.bestslider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 2
+      }
+    }
+  });
+
+ });
+
+var i = 0;
+
+$( ".iconboxone3" ) .mouseover(function() {
+    i += 1;
+     $(".textone3" ).css( "opacity", "1" );
+  })
+  .mouseout(function() {
+    
+     $(".textone3",).css( "opacity", "0" );
+});
+
+var i = 0;
+
+$( ".iconboxone2" ) .mouseover(function() {
+    i += 1;
+     $(".textone2" ).css( "opacity", "1" );
+  })
+  .mouseout(function() {
+    
+     $(".textone2",).css( "opacity", "0" );
+});
+
+$( ".iconboxone1" ) .mouseover(function() {
+    i += 1;
+     $(".textone1" ).css( "opacity", "1" );
+  })
+  .mouseout(function() {
+    
+     $(".textone1",).css( "opacity", "0" );
+});
+
+var n = 0;
+$( ".iconboxtwo3" ).mouseover(function() {
+    n += 1;
+     $(".texttwo3").css( "opacity", "1" );
+  })
+  .mouseout(function() {
+    
+     $(".texttwo3").css( "opacity", "0" );
+});
+ 
+
+var n = 0;
+$( ".iconboxtwo2" ).mouseover(function() {
+    n += 1;
+     $(".texttwo2").css( "opacity", "1" );
+  })
+  .mouseout(function() {
+    
+     $(".texttwo2").css( "opacity", "0" );
+});
+ 
+
+var n = 0;
+$( ".iconboxtwo1" ).mouseover(function() {
+    n += 1;
+     $(".texttwo1").css( "opacity", "1" );
+  })
+  .mouseout(function() {
+    
+     $(".texttwo1").css( "opacity", "0" );
+});
+
+
+
+ 
---
+++ precache-manifest.5ca3865d5f91a6e0b9edaa5f9f188336.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "10fbda3f2e5bd2e6731b2c72644ff873",
+    "url": "/static/media/phone.10fbda3f.svg"
+  },
+  {
+    "revision": "8da4a924ce736785a71d",
+    "url": "/static/css/main.942098e8.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ce89d8437d19472f0f6c",
+    "url": "/static/js/2.cf8e0910.chunk.js"
+  },
+  {
+    "revision": "d04b5dffe80643df665b5d4440141742",
+    "url": "/static/media/vector-2.d04b5dff.svg"
+  },
+  {
+    "revision": "86fa9ec4b9fc2592a35b9ef856af06e2",
+    "url": "/static/media/football.86fa9ec4.svg"
+  },
+  {
+    "revision": "98b6447c35d24df9922f6c1208c27ebd",
+    "url": "/static/media/football2.98b6447c.svg"
+  },
+  {
+    "revision": "94f8219b84f655724b8fc6f0398a10e4",
+    "url": "/static/media/badminton.94f8219b.svg"
+  },
+  {
+    "revision": "8ddd86e2bd0f9363d017968b97156bbf",
+    "url": "/static/media/basketball.8ddd86e2.svg"
+  },
+  {
+    "revision": "7bc023d28e6f2f9b57113b0df956b234",
+    "url": "/static/media/rugby.7bc023d2.svg"
+  },
+  {
+    "revision": "5be9cb591d4f0900084620ad1b5f350d",
+    "url": "/static/media/volleyball.5be9cb59.svg"
+  },
+  {
+    "revision": "9226de8d0971e67727595efd1c9c0301",
+    "url": "/static/media/learn-a-sport-banner-3.9226de8d.jpg"
+  },
+  {
+    "revision": "2a5dee2beff6781630826029b954fedc",
+    "url": "/static/media/book-and-play-banner-3.2a5dee2b.jpg"
+  },
+  {
+    "revision": "f9c281dc192c67e532b82112723d93a4",
+    "url": "/static/media/Polygon.f9c281dc.svg"
+  },
+  {
+    "revision": "ff3ed04faf1febf2fb66bbb6264e5e9f",
+    "url": "/static/media/Polygon-2.ff3ed04f.svg"
+  },
+  {
+    "revision": "8da4a924ce736785a71d",
+    "url": "/static/js/main.e3298578.chunk.js"
+  },
+  {
+    "revision": "8def92a727c583d9417c312921e4ce6e",
+    "url": "/static/media/mail.8def92a7.svg"
+  },
+  {
+    "revision": "3a00bdd2a9f59eccb118cc6016245b5d",
+    "url": "/static/media/office-building-modal.3a00bdd2.svg"
+  },
+  {
+    "revision": "bc60e3029179c960efd57e6a4335488f",
+    "url": "/static/media/shoot-ball-modal.bc60e302.svg"
+  },
+  {
+    "revision": "af12f0b14a651a4433fa4528aabe54e0",
+    "url": "/static/media/Polygon-3.af12f0b1.svg"
+  },
+  {
+    "revision": "1ba7d172c5881a7f02e4eca7a473191a",
+    "url": "/static/media/work.1ba7d172.png"
+  },
+  {
+    "revision": "668e4c93c931acf055aa473695d179e1",
+    "url": "/static/media/car-1.668e4c93.png"
+  },
+  {
+    "revision": "c844c1e16b66313a42ce81df5df582f9",
+    "url": "/static/media/owl-1.c844c1e1.jpg"
+  },
+  {
+    "revision": "fc89781ba4891753d853b4efaab1a3b8",
+    "url": "/static/media/owl-2.fc89781b.jpg"
+  },
+  {
+    "revision": "942a5ee5986025b052ff7a629992f00f",
+    "url": "/static/media/one.942a5ee5.svg"
+  },
+  {
+    "revision": "a88e9fc9f1bb1ac6ade4c258a9c65c70",
+    "url": "/static/media/num.a88e9fc9.svg"
+  },
+  {
+    "revision": "3593de087ccb3c88779bfc61e99b3a4b",
+    "url": "/static/media/coin.3593de08.svg"
+  },
+  {
+    "revision": "a28da46abca4fd33ecde0d3fe71c6a75",
+    "url": "/static/media/partimg.a28da46a.png"
+  },
+  {
+    "revision": "dd8863064d9cde143979b7aa34f78cbd",
+    "url": "/static/media/fts.dd886306.png"
+  },
+  {
+    "revision": "6877eaa1eba9ff8415fb21024834994f",
+    "url": "/index.html"
+  }
+];
