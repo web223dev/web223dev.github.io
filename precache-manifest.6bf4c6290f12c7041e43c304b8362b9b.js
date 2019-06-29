3b15035717a8c77ab47da992de6460e708e8b794
web223dev <ruslanus866@outlook.com>
2019-06-29 12:53:17 +0800

    moviesite

---
+++ precache-manifest.6bf4c6290f12c7041e43c304b8362b9b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4b2c53db3190d5680a0c8f4eac15c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56a70abdf79dc008e71c",
+    "url": "/static/css/2.9f214cd3.chunk.css"
+  },
+  {
+    "revision": "6d73c69dbef9f89c58f2",
+    "url": "/static/css/main.b7e5fadc.chunk.css"
+  },
+  {
+    "revision": "56a70abdf79dc008e71c",
+    "url": "/static/js/2.ae9c6409.chunk.js"
+  },
+  {
+    "revision": "6d73c69dbef9f89c58f2",
+    "url": "/static/js/main.b70a97d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0a7d63d047784e8bc1c23a0ce36d2c2b",
+    "url": "/static/media/Daredevil.0a7d63d0.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
